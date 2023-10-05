228f88e420626956fb751a07585f11d65395aad4
hudaaaamn <hudamuhammadnur@mail.ugm.ac.id>
2023-10-05 14:55:44 +0700

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,59 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Barang</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <style>
-        body {
-            background-color: #f0e68c;
-        }
-        table {
-            background-color: #FFA07A;
-            color: #333; 
-        }
-        table thead th {
-            background-color: #FF6347;
-            color: white; 
-        }
-        table tbody tr:hover {
-            background-color: #FFD700;
-            transition: background-color 0.3s ease-in-out;
-        }
-    </style>
-</head>
-<body>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>id</th>
-                <th>Nama Barang</th>
-                <th>Harga</th>
-                <th>Stok</th>
-                <th>id_supplier</th>
-                <th>Aksi</th>
-            </tr>
+@extends('layout.main')
+
+@section('container')
+    @php
+        use Carbon\Carbon;
+    @endphp
+
+    <h1 class="display-4 ms-5 mt-2 center">Daftar Barang</h1>
+
+    <div class="mt-3 m-5">
+      <table class="table">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">Nama Barang</th>
+            <th scope="col">stok</th>
+            <th scope="col">Harga</th>
+            <th scope="col">id supplier</th>
+          </tr>
         </thead>
         <tbody>
-            @foreach($data_barang as $barang)
-            <tr>
-                <td>{{ $barang->id }}</td>
-                <td>{{ $barang->nama_barang }}</td>
-                <td>{{ $barang->harga }}</td>
-                <td>{{ $barang->stok }}</td>
-                <td>{{ $barang->id_supplier }}</td>
-                <td>
-                    <a href="{{ route('barang.edit', $barang->id) }}"><button class="btn btn-primary">Edit</button></a>
-                    <form action="{{ route('barang.destroy', $barang->id) }}" method="POST" style="display: inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Hapus</button>
-                    </form>
-                </td>
-            </tr>
+            @foreach ($data_barang as $barang)
+                <tr>
+                    <td>{{ ++$no }}</td>
+                    <td>{{ $barang->nama_barang }}</td>
+                    <td>{{ $barang->stok }}</td>
+                    <td>{{ "Rp ".number_format($barang->harga, 2, ',', ',') }}</td>
+                    <td>{{ $barang->id_supplier }}</td>
+                </tr>
             @endforeach
         </tbody>
-    </table>
-</body>
-</html>
+      </table>
+    </div>
+@endsection
